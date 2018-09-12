ec82aac7ec06ff615dae441889b3b348320f1755
Lucas Russo <lerwys@gmail.com>
2018-09-12 11:32:17 -0700

    entrypoint.sh: create and swtich to newly created user

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+# From: https://denibertovic.com/posts/handling-permissions-with-docker-volumes
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+exec /usr/local/bin/gosu user "$@"
